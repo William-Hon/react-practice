b006e9c0a2eb30d792e2ad31d6b92e9372686963
William Hon <32whon@gmail.com>
2025-12-16 13:14:45 -0500

    initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import Navbar from "../components/Navbar.jsx";
+import Hero from "../components/Hero.jsx";
+import HomeCards from "../components/HomeCards.jsx";
+import JobListings from "../components/JobListings.jsx";
+import ViewAllJobs from "../components/ViewAllJobs.jsx";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero title="nxvasaki" subtitle="67777777777"/>
+      <HomeCards />
+      <JobListings isHome={true}/>
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default HomePage;
